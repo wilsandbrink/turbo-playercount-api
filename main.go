0fefb6bef6ccf9b476f02336c2645430f0493c44
William Sandbrink <wilsandbrink@gmail.com>
2019-04-08 15:54:41 +0200

    made some progress

--- main.go
+++ main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
+func getGameID(name string) float64 {
+	data, err := os.Open("data/data.json")
+	if err != nil {
+		panic(err)
+	}
+	defer data.Close()
+	byteValue, _ := ioutil.ReadAll(data)
+
+	var result map[string]interface{}
+	json.Unmarshal([]byte(byteValue), &result)
+
+	b, ok := result[name].(float64)
+
+	if ok {
+		return b
+	}
+	return 404
+}
 
 func main() {
-	fmt.Println("Hello, world!")
+	fmt.Print(getGameID("Rust"))
 }
