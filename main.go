f6fc89cf8ef998237457c8213c65a913ffc9ee99
William Sandbrink <wilsandbrink@gmail.com>
2019-04-08 20:01:47 +0200

    Need to convert something into a slice

--- main.go
+++ main.go
@@ -4,10 +4,17 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"net/http"
 	"os"
 )
 
-func getGameID(name string) float64 {
+type Game struct {
+	name        string
+	id          float64
+	playercount float64
+}
+
+func getGameIDFromName(name string) (string, float64) {
 	data, err := os.Open("data/data.json")
 	if err != nil {
 		panic(err)
@@ -21,11 +28,31 @@ func getGameID(name string) float64 {
 	b, ok := result[name].(float64)
 
 	if ok {
-		return b
+		return name, b
 	}
-	return 404
+	return "notfound", 404
+}
+
+// GetGame takes an id and string and return a game
+func GetGame(id float64, name string) {
+	if id == 404 {
+		//g := Game{"Not found", 404, 0}
+	}
+	url := fmt.Sprintf("https://api.steampowered.com/ISteamUserStats/GetNumberOfCurrentPlayers/v1/?appid=%v", id)
+	resp, err := http.Get(url)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+	byteValue, _ := ioutil.ReadAll(resp.Body)
+
+	var result map[string]interface{}
+	json.Unmarshal([]byte(byteValue), &result)
+	respmap = map[string]interface{}
+	fmt.Print(respmap["player_count"])
 }
 
 func main() {
-	fmt.Print(getGameID("Rust"))
+	name, id := (getGameIDFromName("Rust"))
+	GetGame(id, name)
 }
