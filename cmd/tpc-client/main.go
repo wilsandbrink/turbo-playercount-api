eea41f0f58077cdfabf8f10574d711898c078f61
William Sandbrink <wilsandbrink@gmail.com>
2019-04-11 13:06:56 +0200

    Idk how to fix this mess

--- cmd/tpc-client/main.go
+++ cmd/tpc-client/main.go
@@ -9,21 +9,17 @@ import (
 )
 
 func main() {
-	reader := bufio.NewReader(os.Stdin)
 	for {
+		reader := bufio.NewReader(os.Stdin)
 		fmt.Print("Enter name of game: ")
-		text, _ := reader.ReadString('')
+		text, _ := reader.ReadString('\n')
+		text = text[:len(text)-1]
 		fmt.Println(text)
+		var game = gameutils.GetGame(text)
 		if text == "exit" {
 			os.Exit(0)
 		} else {
-			var game = gameutils.GetGame(text)
-			if game.ID == 404 {
-				fmt.Println("No game by that name.")
-			} else {
-				fmt.Printf("Game: %s, ID: %v, Concurrent players: %v\n", game.Name, game.ID, game.Playercount)
-			}
+			fmt.Printf("Game: %s, ID: %v, Concurrent players: %v\n", game.Name, game.ID, game.Playercount)
 		}
 	}
-
 }
